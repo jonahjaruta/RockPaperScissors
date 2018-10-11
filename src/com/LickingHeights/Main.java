24135dc705bf7d407d8ba3a725bf01dbf371d94d
Jonah <jonah.jaruta.2019@student.lhschools.org>
2018-10-11 08:15:38 -0400

    updated again

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,12 +8,16 @@ public class Main {
 
        //declare
        Scanner keyboard;
+       String selection;
 
 
 
        //initialize
         keyboard = new Scanner(System.in);
 
+        System.out.println("Welcome to Rock, Paper, Scissors!");
+        System.out.println("Go ahead and put your choice in. Rock, paper, or scissors?");
+        selection = keyboard.nextLine();
         
 
 
