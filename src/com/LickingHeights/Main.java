7943519f0cbf5862f942d27445587f10a1f36304
Jonah <jonah.jaruta.2019@student.lhschools.org>
2018-10-11 08:03:18 -0400

    Started the code

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,30 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       //declare
+       Scanner keyboard;
+
+
+
+       //initialize
+        keyboard = new Scanner(System.in);
+
+        
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
