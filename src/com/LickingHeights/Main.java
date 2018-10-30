968adc55954efa87db29760f47d55771d2b2dd4f
Jonah <jonah.jaruta.2019@student.lhschools.org>
2018-10-30 08:11:55 -0400

    Almost Complete
    Add the loops into the game, and a method for keeping score, then code is finished.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -9,30 +9,49 @@ public class Main {
        //declare
        Scanner keyboard;
        String PlayerChoice;
+       Integer ComputerNumber;
        String ComputerChoice;
+       String[] Options;
 
-       
 
 
        //initialize
         keyboard = new Scanner(System.in);
 
-        System.out.println("Welcome to Rock, Paper, Scissors!");
+        System.out.println("Welcome to rock, paper, scissors!");
         System.out.println("Go ahead and put your choice in. rock, paper, or scissors?");
         PlayerChoice = keyboard.nextLine();
 
+        Options = new String[3];
+        Options[0] = "rock";
+        Options[1] = "paper";
+        Options[2] = "scissors";
 
+        ComputerChoice = Options[(int) (Math.random() * Options.length)];
 
-
-
-
-
-
-
-
-
-
-
+        if (PlayerChoice.equals(ComputerChoice)) {
+            System.out.println("It's a tie!");
+        }
+        else if (PlayerChoice.equals("rock")) {
+            if (ComputerChoice.equals("scissors"))
+                System.out.println("Rock beats scissors, You win!");
+            else if (ComputerChoice.equals("paper"))
+                System.out.println("Paper beats rock, You lose.");
+        }
+        else if (PlayerChoice.equals("paper")) {
+            if (ComputerChoice.equals("scissors"))
+                System.out.println("Scissor beats paper, You lose.");
+            else if (ComputerChoice.equals("rock"))
+                System.out.println("Paper beats rock, You win!");
+        }
+        else if (PlayerChoice.equals("scissors")) {
+            if (ComputerChoice.equals("paper"))
+                System.out.println("Scissor beats paper, You win!");
+            else if (ComputerChoice.equals("rock"))
+                System.out.println("Rock beats scissors, You lose.");
+        }
+        else
+            System.out.println("Invalid user input, try again.");
 
 
 
