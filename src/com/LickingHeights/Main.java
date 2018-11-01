517aaf0f8d5ca1b5eb941abcceaaaec6215acf57
Jonah <jonah.jaruta.2019@student.lhschools.org>
2018-11-01 08:53:32 -0400

    One Stage Left

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,54 +6,78 @@ public class Main {
 
     public static void main(String[] args) {
 
-       //declare
-       Scanner keyboard;
-       String PlayerChoice;
-       Integer ComputerNumber;
-       String ComputerChoice;
-       String[] Options;
+        //declare
+        Scanner keyboard;
+        String PlayerChoice;
+        Integer ComputerNumber;
+        String ComputerChoice;
+        String[] Options;
+        String PlayAgain = null;
+        boolean runAgain = false;
 
 
-
-       //initialize
+        //initialize
         keyboard = new Scanner(System.in);
 
-        System.out.println("Welcome to rock, paper, scissors!");
-        System.out.println("Go ahead and put your choice in. rock, paper, or scissors?");
-        PlayerChoice = keyboard.nextLine();
+        while (runAgain == false) {
 
-        Options = new String[3];
-        Options[0] = "rock";
-        Options[1] = "paper";
-        Options[2] = "scissors";
+            boolean win = false;
+            int score = 0;
 
-        ComputerChoice = Options[(int) (Math.random() * Options.length)];
 
-        if (PlayerChoice.equals(ComputerChoice)) {
-            System.out.println("It's a tie!");
+            System.out.println("Welcome to rock, paper, scissors! Try to reach 3 points!");
+            while (win == false) {
+                System.out.println("Go ahead and put your choice in. rock, paper, or scissors?");
+                PlayerChoice = keyboard.nextLine();
+
+                Options = new String[3];
+                Options[0] = "rock";
+                Options[1] = "paper";
+                Options[2] = "scissors";
+
+                ComputerChoice = Options[(int) (Math.random() * Options.length)];
+
+                if (PlayerChoice.equals(ComputerChoice)) {
+                    System.out.println("It's a tie!");
+                } else if (PlayerChoice.equals("rock")) {
+                    if (ComputerChoice.equals("scissors")) {
+                        System.out.println("Rock beats scissors, You win!");
+                        score++;
+                    }
+                    if (ComputerChoice.equals("paper"))
+                        System.out.println("Paper beats rock, You lose.");
+                } else if (PlayerChoice.equals("paper")) {
+                    if (ComputerChoice.equals("scissors"))
+                        System.out.println("Scissor beats paper, You lose.");
+                    else if (ComputerChoice.equals("rock")) {
+                        System.out.println("Paper beats rock, You win!");
+                        score++;
+                    }
+                } else if (PlayerChoice.equals("scissors")) {
+                    if (ComputerChoice.equals("paper")) {
+                        System.out.println("Scissor beats paper, You win!");
+                        score++;
+                    } else if (ComputerChoice.equals("rock"))
+                        System.out.println("Rock beats scissors, You lose.");
+                } else
+                    System.out.println("Invalid user input, try again.");
+                System.out.println("Your score is " + score + ".");
+
+                if (score == 3) {
+                    win = true;
+                    System.out.println("want to play again? (Y) for yes (N) for no.");
+                    PlayAgain = keyboard.nextLine();
+                }
+                if (PlayAgain == "N") {
+                    runAgain = true;
+                }
+            }
+
         }
-        else if (PlayerChoice.equals("rock")) {
-            if (ComputerChoice.equals("scissors"))
-                System.out.println("Rock beats scissors, You win!");
-            else if (ComputerChoice.equals("paper"))
-                System.out.println("Paper beats rock, You lose.");
-        }
-        else if (PlayerChoice.equals("paper")) {
-            if (ComputerChoice.equals("scissors"))
-                System.out.println("Scissor beats paper, You lose.");
-            else if (ComputerChoice.equals("rock"))
-                System.out.println("Paper beats rock, You win!");
-        }
-        else if (PlayerChoice.equals("scissors")) {
-            if (ComputerChoice.equals("paper"))
-                System.out.println("Scissor beats paper, You win!");
-            else if (ComputerChoice.equals("rock"))
-                System.out.println("Rock beats scissors, You lose.");
-        }
-        else
-            System.out.println("Invalid user input, try again.");
-
-
-
     }
+
+
 }
+
+
+
